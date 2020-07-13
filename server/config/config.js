fe828374e8873115153629d88e979ad258a9eb08
Andres Lopez <andreslo2612@gmail.com>
2020-07-12 20:00:36 -0500

    RestSERVER - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -4,3 +4,22 @@
 
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+//  Entorno  //
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+
+// Base de datos //
+
+let urlDB;
+
+if( process.env.NODE_ENV === 'dev' ){
+    urlDB = 'mongodb://localhost:27017/curso';
+}else{
+    urlDB = 'mongodb+srv://AndresLo:fGpfMhHOATJqHEog@proyectos.2liaq.mongodb.net/curso';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -9,41 +11,26 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
-app.use(bodyParser.json())
- 
-app.get('/usuario', function (req, res) {
-  res.send('getUsuario')
-});
+app.use(bodyParser.json());
 
-app.post('/usuario', function (req, res) {
+app.use( require('./routes/usuario'));
 
-  let body = req.body;
-
-  if( body.nombre === undefined ){
-    res.status(400).json({
-      ok: false,
-      mensaje: 'El nombre es necesarios'
+const connectDB = async() => {
+  try{
+    await mongoose.connect(process.env.URLDB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
     });
-  }else{
-    res.json({
-      persona: body
-    });
+    return 'Conectado a la base datos';
+  } catch(e){
+    return `${ e }`;
   }  
-});
+}
 
-app.put('/usuario/:id', function (req, res) {
-  
-  let id = req.params.id;
-  
-  res.json({
-    id
-  });
-});
-
-app.delete('/usuario', function (req, res) {
-  res.send('deleteUsuario')
-});
+connectDB()
+      .then( console.log)
+      .catch( console.log )
  
 app.listen(process.env.PORT, () => {
     console.log('Esuchando puerto ',process.env.PORT);
-})
+});
